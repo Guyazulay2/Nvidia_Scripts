92273d238676082b11818b392fd00da4d71cc68a
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2020-10-20 14:17:12 +0300

    Update Nvidia-installation.sh

--- Nvidia-installation.sh
+++ Nvidia-installation.sh
@@ -7,8 +7,9 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
+sudo apt upgrade -y 
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install docker-ce -y 
 
 echo "** Docker installed Successfully **"
 
