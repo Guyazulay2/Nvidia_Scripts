c7ffbbee94fdf07b2d31825007bca3c4408fcee8
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2020-10-26 12:30:33 +0200

    Update Nvidia-installation.sh

--- Nvidia-installation.sh
+++ Nvidia-installation.sh
@@ -18,7 +18,7 @@ if [ "$(dpkg -l | awk '/gstreamer/ {print }'|wc -l)" -ge 1 ]; then
 
 else
 
-        echo -e "Gstreamer Not installed !/n Installing now ..."
+        echo -e "Gstreamer Not installed !\n Installing now ..."
         sudo apt-get update
         sudo apt-get install -y gstreamer1.0-tools gstreamer1.0-nice gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-pl$
         echp "Done !"
@@ -32,15 +32,11 @@ if [ "$(dpkg -l | awk '/cuda-10-2/ {print }'|wc -l)" -ge 1 ]; then
         sleep 1
 else
 
-        echo -e "Cuda Not installed !/n installing now cuda 10.2 >>"
+        echo -e "Cuda Not installed !\n installing now cuda 10.2 >>"
         wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-
         sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-
         wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-
         sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-
         sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
         sudo apt-get update
         sudo apt-get -y install cuda
@@ -55,15 +51,11 @@ if [ "$(dpkg -l | awk '/nvidia-docker2/ {print }'|wc -l)" -ge 1 ]; then
         sleep 1
 else
 
-        echo "<< Install nvidia-docker >>"
+        echo -e "Nvidia-Docker-2 not exsist ! installing now ... >>"
         sleep 2
         distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-
         curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-
         curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-
-
         sudo apt-get update && sudo apt-get install -y nvidia-docker2
         sudo systemctl restart docker
         sleep 2; nvidia-smi
